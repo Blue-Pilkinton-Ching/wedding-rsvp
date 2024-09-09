1cb380cb83df714d6936ecec50d648f1b85d1495
Blue Pilkinton-Ching <bluepilkintonching@gmail.com>
2024-09-09 17:40:48 +1200

    update navbar

--- src/routes/[permission]/+layout.svelte
+++ src/routes/[permission]/+layout.svelte
@@ -1,15 +1,24 @@
 <script>
 	export let data;
 	import NavItem from './NavItem.svelte';
+	import { page } from '$app/stores';
 </script>
 
 <main class="h-dvh">
 	<nav class="w-full p-6">
 		<ul class="gap-6 flex flex-row list-none text-xl font-lato font-light">
-			<NavItem href={`/${data.permission}/home`} label="HOME" />
-			<NavItem href={`/${data.permission}/rsvp`} label="RSVP" />
-			<NavItem href={`/${data.permission}/schedule`} label="SCHEDULE" />
-			<NavItem href={`/${data.permission}/contact`} label="CONTACT" />
+			<NavItem href={`/${data.permission}/home`} label="HOME" currentPath={$page.url.pathname} />
+			<NavItem href={`/${data.permission}/rsvp`} label="RSVP" currentPath={$page.url.pathname} />
+			<NavItem
+				href={`/${data.permission}/schedule`}
+				label="SCHEDULE"
+				currentPath={$page.url.pathname}
+			/>
+			<NavItem
+				href={`/${data.permission}/contact`}
+				label="CONTACT"
+				currentPath={$page.url.pathname}
+			/>
 		</ul>
 	</nav>
 </main>
--- src/routes/[permission]/NavItem.svelte
+++ src/routes/[permission]/NavItem.svelte
@@ -1,13 +1,26 @@
 <script>
 	export let href;
 	export let label;
+	export let currentPath;
+
+	$: isActive = currentPath === href;
 </script>
 
 <li class="relative">
 	<a
 		{href}
 		class="after:absolute after:h-[1px] after:w-0 after:bg-black after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300"
+		class:active={isActive}
 	>
-		{label}
+		<span class="relative">
+			<span class="absolute left-0">{label}</span>
+			<span class="left-0 invisible font-normal" aria-hidden="true">{label}}</span>
+		</span>
 	</a>
 </li>
+
+<style>
+	.active span {
+		font-weight: normal;
+	}
+</style>
