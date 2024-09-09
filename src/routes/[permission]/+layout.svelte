518d3dbf2b57dffa7938d4eb4baa22b5731e9be0
Blue Pilkinton-Ching <bluepilkintonching@gmail.com>
2024-09-09 17:25:52 +1200

    added navbar

---
+++ src/routes/[permission]/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let data;
+	import NavItem from './NavItem.svelte';
+</script>
+
+<main class="h-dvh">
+	<nav class="w-full p-6">
+		<ul class="gap-6 flex flex-row list-none text-xl font-lato font-light">
+			<NavItem href={`/${data.permission}/home`} label="HOME" />
+			<NavItem href={`/${data.permission}/rsvp`} label="RSVP" />
+			<NavItem href={`/${data.permission}/schedule`} label="SCHEDULE" />
+			<NavItem href={`/${data.permission}/contact`} label="CONTACT" />
+		</ul>
+	</nav>
+</main>
---
+++ src/routes/[permission]/NavItem.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let href;
+	export let label;
+</script>
+
+<li class="relative">
+	<a
+		{href}
+		class="after:absolute after:h-[1px] after:w-0 after:bg-black after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300"
+	>
+		{label}
+	</a>
+</li>
